8645ed1abf2e7e198e5c055e3c7261a9b1e5b959
thiennt <thiennt@senprints.com>
2023-12-23 19:18:18 +0700

    cors

--- src/app.js
+++ src/app.js
@@ -2,14 +2,21 @@ import express from "express"
 import morgan from "morgan"
 import helmet from "helmet"
 import compression from "compression"
+import cors from "cors"
 import publicRouter from "./routers/public.js"
 import 'dotenv/config'
 
 const app = express()
 
 // init middleware
-// app.use(morgan("combined"))
-app.use(morgan("dev")) //log
+const morgranFormat = process.env.NODE_ENV === "production" ? "combined" : "dev"
+const corsOptions = {
+  origin: ['http://localhost:5173', 'https://spirit-companion.netlify.app', 'https://main--ark-nova-helper.netlify.app'],
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions)) //cors
+app.use(morgan(morgranFormat)) //log
 app.use(helmet()) //security
 app.use(compression()) //compress
 app.use(express.json()); // parse json
--- src/configs/mongodb.js
+++ src/configs/mongodb.js
@@ -1,6 +1,6 @@
 const config = {
   dev: process.env.MONGODB_URI_DEV,
-  prod: process.env.MONGODB_URI_PROD
+  production: process.env.MONGODB_URI_PROD
 }
 
 const env = process.env.NODE_ENV || 'dev'
